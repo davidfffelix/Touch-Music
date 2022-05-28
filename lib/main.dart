ffda39653dcb3c975e55056afaa14fa974d150a5
David Felix <davidffelix@hotmail.com>
2022-05-27 21:25:30 -0300

    Insert new library

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(MarimbaApp());
 
 class MarimbaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
