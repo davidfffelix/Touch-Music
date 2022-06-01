c1d245435918ddbc9e4a644a492a7d8d08836de8
David Felix <davidffelix@hotmail.com>
2022-06-01 12:06:02 -0300

    Buttons customization

--- lib/main.dart
+++ lib/main.dart
@@ -15,78 +15,100 @@ class MarimbaApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Center(
             child: Column(
               children: [
-                TextButton(
-                  style: TextButton.styleFrom(
-                    backgroundColor: Color(0xff303F9F),
-                  ),
-                  onPressed: () {
-                    playSound(1);
-                  },
-                ),
-
-                TextButton(
-                  style: TextButton.styleFrom(
-                    backgroundColor: Color(0xffffd600
+                Expanded(
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Color(0xff303F9F),
                     ),
-                  ),
-                  onPressed: (){
-                    playSound(2);
-                  },
-                ),
-
-                TextButton(
-                  style: TextButton.styleFrom(
-                    backgroundColor: Color(0xff00c853
-                    ),
-                  ),
-                    onPressed: (){
-                      playSound(3);
+                    onPressed: () {
+                      playSound(1);
                     },
+                    child: Text('Clique em mim!'),
+                  ),
                 ),
 
-                TextButton(
-                  style: TextButton.styleFrom(
-                    backgroundColor: Color(0xffff6d00
+                Expanded(
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Color(0xffffd600
+                      ),
                     ),
+                    onPressed: (){
+                      playSound(2);
+                    },
+                    child: Text('Clique em mim 2!'),
                   ),
-                  onPressed: (){
-                    playSound(4);
-                  },
                 ),
 
-                TextButton(
-                  style: TextButton.styleFrom(
-                    backgroundColor: Color(0xff6200ea
+                Expanded(
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Color(0xff00c853
+                      ),
                     ),
+                      onPressed: (){
+                        playSound(3);
+                      },
+                      child: Text('Clique em mim 3!'),
                   ),
-                  onPressed: (){
-                    playSound(5);
-                  },
                 ),
 
-                TextButton(
-                  style: TextButton.styleFrom(
-                    backgroundColor: Color(0xff757575
-                   ),
+                Expanded(
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Color(0xffff6d00
+                      ),
+                    ),
+                    onPressed: (){
+                      playSound(4);
+                    },
+                    child: Text('Clique em mim 4!'),
                   ),
-                  onPressed: (){
-                    playSound(6);
-                  },
                 ),
 
-                TextButton(
-                  style: TextButton.styleFrom(
-                    backgroundColor: Color(0xff212121),
-                    shape: StadiumBorder(),
-                    padding: EdgeInsets.all(12),
+                Expanded(
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Color(0xff6200ea
+                      ),
+                    ),
+                    onPressed: (){
+                      playSound(5);
+                    },
+                    child: Text('Clique em mim 5!'),
                   ),
-                  onPressed: (){
-                    playSound(7);
-                  },
+                ),
+
+                Expanded(
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Color(0xff757575
+                     ),
+                    ),
+                    onPressed: (){
+                      playSound(6);
+                    },
+                    child: Text('Clique em mim 6!'),
+                  ),
+                ),
+
+                Expanded(
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Color(0xff212121),
+                      shape: StadiumBorder(),
+                      padding: EdgeInsets.all(12),
+                    ),
+                    onPressed: (){
+                      playSound(7);
+                    },
+                    child: Text('Clique em mim 7!'),
+                  ),
                 ),
               ],
             ),
