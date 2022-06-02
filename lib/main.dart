c35d13b7e9a8b36d20eadd98878d8f6496f904a2
David Felix <davidffelix@hotmail.com>
2022-06-02 18:45:23 -0300

    Custom code

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,20 @@ class MarimbaApp extends StatelessWidget {
     player.play('nota$numberSound.wav'); // Reproduz o arquivo de áudio escolhido
   }
 
+  void createButton(){
+    Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(
+          backgroundColor: Color(0xff303F9F),
+        ),
+        onPressed: () {
+          playSound(1);
+        },
+        child: Container(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,94 +33,13 @@ class MarimbaApp extends StatelessWidget {
         body: SafeArea(
           child: Column(
             children: [
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    backgroundColor: Color(0xff303F9F),
-                  ),
-                  onPressed: () {
-                    playSound(1);
-                  },
-                  child: Container(),
-                ),
-              ),
-
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    backgroundColor: Color(0xffffd600
-                    ),
-                  ),
-                  onPressed: (){
-                    playSound(2);
-                  },
-                  child: Container(),
-                ),
-              ),
-
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    backgroundColor: Color(0xff00c853
-                    ),
-                  ),
-                    onPressed: (){
-                      playSound(3);
-                    },
-                    child: Container(),
-                ),
-              ),
-
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    backgroundColor: Color(0xffff6d00
-                    ),
-                  ),
-                  onPressed: (){
-                    playSound(4);
-                  },
-                  child: Container(),
-                ),
-              ),
-
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    backgroundColor: Color(0xfffafafa
-                    ),
-                  ),
-                  onPressed: (){
-                    playSound(5);
-                  },
-                  child: Container(),
-                ),
-              ),
-
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    backgroundColor: Color(0xffd50000
-                   ),
-                  ),
-                  onPressed: (){
-                    playSound(6);
-                  },
-                  child: Container(),
-                ),
-              ),
-
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    backgroundColor: Color(0xff212121),
-                  ),
-                  onPressed: (){
-                    playSound(7);
-                  },
-                  child: Container(),
-                ),
-              ),
+              createButton(),
+              createButton(),
+              createButton(),
+              createButton(),
+              createButton(),
+              createButton(),
+              createButton(),
             ],
           ),
         ),
