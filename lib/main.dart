ea1ab6d0990fced2a753c52a5884b1fd4875f758
David Felix <davidffelix@hotmail.com>
2022-06-02 18:37:23 -0300

    Update layout

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,8 @@ void main() => runApp(MarimbaApp());
 class MarimbaApp extends StatelessWidget {
 
   void playSound(int numberSound){
-    final player = AudioCache(); // Executa o áudio dos arquivos locais
-    player.play('nota$numberSound.wav');
+    final player = AudioCache(); // Executa os áudios do arquivo local
+    player.play('nota$numberSound.wav'); // Reproduz o arquivo de áudio escolhido
   }
 
   @override
@@ -17,101 +17,97 @@ class MarimbaApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: Column(
-              children: [
-                Expanded(
-                  child: TextButton(
-                    style: TextButton.styleFrom(
-                      backgroundColor: Color(0xff303F9F),
-                    ),
-                    onPressed: () {
-                      playSound(1);
-                    },
-                    child: Text('Clique em mim!'),
+          child: Column(
+            children: [
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Color(0xff303F9F),
                   ),
+                  onPressed: () {
+                    playSound(1);
+                  },
+                  child: Container(),
                 ),
+              ),
 
-                Expanded(
-                  child: TextButton(
-                    style: TextButton.styleFrom(
-                      backgroundColor: Color(0xffffd600
-                      ),
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Color(0xffffd600
                     ),
+                  ),
+                  onPressed: (){
+                    playSound(2);
+                  },
+                  child: Container(),
+                ),
+              ),
+
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Color(0xff00c853
+                    ),
+                  ),
                     onPressed: (){
-                      playSound(2);
+                      playSound(3);
                     },
-                    child: Text('Clique em mim 2!'),
-                  ),
+                    child: Container(),
                 ),
+              ),
 
-                Expanded(
-                  child: TextButton(
-                    style: TextButton.styleFrom(
-                      backgroundColor: Color(0xff00c853
-                      ),
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Color(0xffff6d00
                     ),
-                      onPressed: (){
-                        playSound(3);
-                      },
-                      child: Text('Clique em mim 3!'),
                   ),
+                  onPressed: (){
+                    playSound(4);
+                  },
+                  child: Container(),
                 ),
+              ),
 
-                Expanded(
-                  child: TextButton(
-                    style: TextButton.styleFrom(
-                      backgroundColor: Color(0xffff6d00
-                      ),
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Color(0xfffafafa
                     ),
-                    onPressed: (){
-                      playSound(4);
-                    },
-                    child: Text('Clique em mim 4!'),
                   ),
+                  onPressed: (){
+                    playSound(5);
+                  },
+                  child: Container(),
                 ),
+              ),
 
-                Expanded(
-                  child: TextButton(
-                    style: TextButton.styleFrom(
-                      backgroundColor: Color(0xff6200ea
-                      ),
-                    ),
-                    onPressed: (){
-                      playSound(5);
-                    },
-                    child: Text('Clique em mim 5!'),
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Color(0xffd50000
+                   ),
                   ),
+                  onPressed: (){
+                    playSound(6);
+                  },
+                  child: Container(),
                 ),
+              ),
 
-                Expanded(
-                  child: TextButton(
-                    style: TextButton.styleFrom(
-                      backgroundColor: Color(0xff757575
-                     ),
-                    ),
-                    onPressed: (){
-                      playSound(6);
-                    },
-                    child: Text('Clique em mim 6!'),
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Color(0xff212121),
                   ),
+                  onPressed: (){
+                    playSound(7);
+                  },
+                  child: Container(),
                 ),
-
-                Expanded(
-                  child: TextButton(
-                    style: TextButton.styleFrom(
-                      backgroundColor: Color(0xff212121),
-                      shape: StadiumBorder(),
-                      padding: EdgeInsets.all(12),
-                    ),
-                    onPressed: (){
-                      playSound(7);
-                    },
-                    child: Text('Clique em mim 7!'),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
