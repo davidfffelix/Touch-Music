3c1a0e3b5ee2b955ba17bd7560d9cf19f67b64c6
David Felix <davidffelix@hotmail.com>
2022-06-04 10:37:15 -0300

    Correction of class name and input data

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
-void main() => runApp(MarimbaApp());
+void main() => runApp(TouchMusic());
 
-class MarimbaApp extends StatelessWidget {
+class TouchMusic extends StatelessWidget {
   void playSound(int numberSound) {
     final player = AudioCache(); // Executa os áudios do arquivo local
     player
         .play('nota$numberSound.wav'); // Reproduz o arquivo de áudio escolhido
   }
 
-  Expanded createButton({required Color color, required int numberNote}) {
+  Expanded createButton({required Color color, required int noteNumber}) {
     return Expanded(
       child: TextButton(
         style: TextButton.styleFrom(
           backgroundColor: color,
         ),
         onPressed: () {
-          playSound(numberNote);
+          playSound(noteNumber);
         },
         child: Container(),
       ),
@@ -33,13 +33,13 @@ class MarimbaApp extends StatelessWidget {
         body: SafeArea(
           child: Column(
             children: [
-              createButton(color: Color(0xff1a237e), numberNote: 1),
-              createButton(color: Color(0xffffff00), numberNote: 2),
-              createButton(color: Color(0xff00e676), numberNote: 3),
-              createButton(color: Color(0xffff3d00), numberNote: 4),
-              createButton(color: Color(0xff6a1b9a), numberNote: 5),
-              createButton(color: Color(0xfffafafa), numberNote: 6),
-              createButton(color: Color(0xffd50000), numberNote: 7),
+              createButton(color: Color(0xff1a237e), noteNumber: 1),
+              createButton(color: Color(0xffffff00), noteNumber: 2),
+              createButton(color: Color(0xff00e676), noteNumber: 3),
+              createButton(color: Color(0xffff3d00), noteNumber: 4),
+              createButton(color: Color(0xff6a1b9a), noteNumber: 5),
+              createButton(color: Color(0xfffafafa), noteNumber: 6),
+              createButton(color: Color(0xffd50000), noteNumber: 7),
             ],
           ),
         ),
